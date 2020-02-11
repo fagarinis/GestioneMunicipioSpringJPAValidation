192bbe0bce5d96d328c35a44dcb46f331cfbacf4
Claudia Esposito <claudia.esposito258@gmail.com>
2020-02-11 16:24:56 +0100

    Commit Delete Municipio

--- WebContent/municipio/delete.jsp
+++ WebContent/municipio/delete.jsp
@@ -1,12 +1,51 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+	pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="ISO-8859-1">
-<title>Insert title here</title>
+<title>Cancella Municipio</title>
 </head>
 <body>
+	<div class="container">
+		<%@ include file="../header.jsp"%>
+
+		<div class="page-header">
+			<h3>Pagina di Cancellazione</h3>
+		</div>
+
+
+		<form action="ExecuteEliminaMunicipioServlet" method="post">
+			<div class="container-fluid">
+				<dl class="row">
+					<dt class="col-sm-3 text-right">Id</dt>
+					<dd class="col-sm-9">${municipioAttr.id }</dd>
+				</dl>
+				<dl class="row">
+					<dt class="col-sm-3 text-right">Descrizione</dt>
+					<dd class="col-sm-9">${municipioAttr.descrizione }</dd>
+				</dl>
+				<dl class="row">
+					<dt class="col-sm-3 text-right">Codice</dt>
+					<dd class="col-sm-9">${municipioAttr.codice }</dd>
+				</dl>
+				<dl class="row">
+					<dt class="col-sm-3 text-right">Ubicazione</dt>
+					<dd class="col-sm-9">${municipioAttr.ubicazione }</dd>
+				</dl>
+			</div>
+
+			<input type=hidden name="idMunicipio"
+				value="${municipioAttr.id }">
+			<div class="form-group">
+				<div class="col-sm-offset-2 col-sm-10">
+					<button type="submit" class="btn btn-primary btn-md">Cancella</button>
+				</div>
+			</div>
+			<%@ include file="../footer.jsp"%>
+		</form>
+	</div>
 
 </body>
 </html>
--- src/it/prova/gestionemunicipiospringjpa/web/servlet/municipio/ExecuteEliminaMunicipioServlet.java
+++ src/it/prova/gestionemunicipiospringjpa/web/servlet/municipio/ExecuteEliminaMunicipioServlet.java
@@ -1,11 +1,20 @@
 package it.prova.gestionemunicipiospringjpa.web.servlet.municipio;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
+
+import it.prova.gestionemunicipiospringjpa.model.Municipio;
+import it.prova.gestionemunicipiospringjpa.service.municipio.MunicipioService;
 
 /**
  * Servlet implementation class ExecuteEliminaMunicipioServlet
@@ -13,29 +22,54 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/ExecuteEliminaMunicipioServlet")
 public class ExecuteEliminaMunicipioServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ExecuteEliminaMunicipioServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	@Autowired
+	private MunicipioService municipioService;
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
+	}
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public ExecuteEliminaMunicipioServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String id = request.getParameter("idMunicipio");
+		Municipio municipio = municipioService.caricaSingoloMunicipio(Long.valueOf(id));
+
+		try {
+
+			municipioService.rimuovi(municipio);
+			request.setAttribute("listaMunicipi", municipioService.listAllMunicipi());
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		RequestDispatcher rd = request.getRequestDispatcher("/municipio/results.jsp");
+		rd.forward(request, response);
+
 	}
 
 }
