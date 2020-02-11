4953e092a4b9e119beb36f5aca06a220e50e7f7e
Ciro Trentacosti <cirotrent@gmail.com>
2020-02-11 14:30:12 +0100

    aggiunta di modifica jsp

--- src/it/prova/gestionemunicipiospringjpa/web/servlet/municipio/PrepareModificaMunicipioServlet.java
+++ src/it/prova/gestionemunicipiospringjpa/web/servlet/municipio/PrepareModificaMunicipioServlet.java
@@ -1,41 +1,55 @@
 package it.prova.gestionemunicipiospringjpa.web.servlet.municipio;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class PrepareModificaMunicipioServlet
- */
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
+
+import it.prova.gestionemunicipiospringjpa.model.Municipio;
+import it.prova.gestionemunicipiospringjpa.service.municipio.MunicipioService;
+
 @WebServlet("/PrepareModificaMunicipioServlet")
 public class PrepareModificaMunicipioServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public PrepareModificaMunicipioServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	@Autowired
+	private MunicipioService municipioService;
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	public PrepareModificaMunicipioServlet() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		if (request.getSession().getAttribute("userInfo") == null) {
+			response.sendRedirect(request.getContextPath());
+			return;
+		}
+
+		String parametroIdMunicipioDaModificare = request.getParameter("idMunicipio");
+		Municipio result = municipioService.caricaSingoloMunicipio(Long.parseLong(parametroIdMunicipioDaModificare));
+
+		request.setAttribute("municipioAttribute", result);
+		RequestDispatcher rd = request.getRequestDispatcher("/municipio/modifica.jsp");
+		rd.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 	}
 
 }
