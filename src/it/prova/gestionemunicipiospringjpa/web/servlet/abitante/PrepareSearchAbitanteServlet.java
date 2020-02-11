5f70f177ebfb29bc78ced22c12f2197b1cb82829
Federico Agarinis <federicof1994@gmail.com>
2020-02-11 14:28:44 +0100

    search.jsp result.jsp di abitante

--- src/it/prova/gestionemunicipiospringjpa/web/servlet/abitante/PrepareSearchAbitanteServlet.java
+++ src/it/prova/gestionemunicipiospringjpa/web/servlet/abitante/PrepareSearchAbitanteServlet.java
@@ -1,11 +1,21 @@
 package it.prova.gestionemunicipiospringjpa.web.servlet.abitante;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
+
+import it.prova.gestionemunicipiospringjpa.model.Municipio;
+import it.prova.gestionemunicipiospringjpa.service.municipio.MunicipioService;
 
 /**
  * Servlet implementation class PrepareSearchAbitanteServlet
@@ -13,29 +23,49 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/PrepareSearchAbitanteServlet")
 public class PrepareSearchAbitanteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public PrepareSearchAbitanteServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	@Autowired
+	private MunicipioService municipioService;
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	public PrepareSearchAbitanteServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// controllo utente in sessione (va fatto in tutte le servlet)
+		if (request.getSession().getAttribute("userInfo") == null) {
+			response.sendRedirect(request.getContextPath());
+			return;
+		}
+
+		List<Municipio> municipi = municipioService.listAllMunicipi();
+		request.setAttribute("listaMunicipiAttributeName", municipi);
+
+		RequestDispatcher rd = request.getRequestDispatcher("/abitante/search.jsp");
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 	}
 
 }
